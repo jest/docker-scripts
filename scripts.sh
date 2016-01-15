dcde6a06abc7227ed60793547f7bfd6f8cdd421c
Przemek Wesołek <jest@go.art.pl>
2016-01-15 08:55:04 +0100

    Added a function to list container IDs of a Rancher service.

--- scripts.sh
+++ scripts.sh
@@ -8,8 +8,19 @@ s-d-c () (
 	STAGE="${stage}" docker-compose -p "${proj}_${stage}" -f docker-compose.yml -f docker-compose-"${stage}".yml "$@"
 )
 
+# Finds ALL rancher service container ID given its Rancher service name
+# if no name is given, returns IDs of all Rancher service contaiers
+__find_rancher_service_container () {
+	local srv=$1
+	if [[ -n "$srv" ]]; then
+		srv="=$srv"
+	fi
+	docker ps -f label=io.rancher.project_service.name"$srv" -q
+}
+
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source "$DIR/dckvol.sh"
+source "$DIR"/dckvol.sh
 
 # vim:ts=2:sw=2:filetype=sh:
 # -*- mode: bash; tab-width: 2 -*-
